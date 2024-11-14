7167232a169c37de49542623e8cf12ee92341bbc
Apaar <161523898+dotslashapaar@users.noreply.github.com>
2024-11-14 10:24:42 +0530

    PDA files added

---
+++ 03-PDA-createProgramAddress/client/client.ts
@@ -0,0 +1,12 @@
+import { PublicKey } from "@solana/web3.js";
+
+const programId = new PublicKey("11111111111111111111111111111111");
+const string = "helloWorld";
+const bump = 254;
+
+const PDA = PublicKey.createProgramAddressSync(
+  [Buffer.from(string), Buffer.from([bump])],
+  programId
+);
+
+console.log(`PDA: ${PDA}`);
---
+++ 04-PDA-findAllConicalBump/client/client.ts
@@ -0,0 +1,17 @@
+import { PublicKey } from "@solana/web3.js";
+
+const programId = new PublicKey("11111111111111111111111111111111");
+const string = "helloWorld";
+
+// Loop through all bump seeds for demonstration
+for (let bump = 255; bump >= 0; bump--) {
+  try {
+    const PDA = PublicKey.createProgramAddressSync(
+      [Buffer.from(string), Buffer.from([bump])],
+      programId
+    );
+    console.log("bump " + bump + ": " + PDA);
+  } catch (error) {
+    console.log("bump " + bump + ": " + error);
+  }
+}
